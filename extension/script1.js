3e64832cff96a407ddf4667f562af140e7a46828
aaafgcfg <aaafgcfg@gmail.com>
2020-06-22 13:52:01 +0300

    +

--- extension/script1.js
+++ extension/script1.js
@@ -1,2 +1,32 @@
 // JavaScript source code
 // 
+
+
+//document.querySelector("div div.f4 span");
+
+var executed= (a)=>{
+	alert(a);
+}
+
+
+chrome.tabs.executeScript(null,
+{
+"code":`
+
+let ret={
+"question":"",
+"answer":""
+};
+
+
+ret.question = document.querySelector("fieldset legend").innerText;
+let varis = document.querySelectorAll("fieldset table tbody tr");
+for( let one of varis){
+	if(one.querySelector("input").checked){
+		ret.answer+=one.innerText+"\n";
+	}
+}
+ret;
+`
+},executed);
+
